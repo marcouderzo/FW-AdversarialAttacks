7908e108f7bb5f6173427d9eccbafbd3eeaf4ef3
Marco Uderzo <marco.uderzodj@gmail.com>
2023-10-25 12:28:59 +0200

    using basis vector correctly, still zero in perturbation. Check params

--- Universal_Attack.py
+++ Universal_Attack.py
@@ -93,11 +93,9 @@ if __name__ == "__main__":
     parser.add_argument('-L', type=float, default=1, help="L (Lipschitz constant) parameter for FZCGS")
     parser.add_argument('-nFunc', type=int, default=10, help="Number of images being attacked at once")
     parser.add_argument('-target_label', type=int, default=1, help="The target digit to attack")
-
-    # Probably not used for our project
     parser.add_argument('-alpha', type=float, default=1.0, help="Optimizer's step size being (alpha)/(input image size)")
     parser.add_argument('-M', type=int, default=50, help="Length of each stage/epoch")
-    parser.add_argument('-nStage', type=int, default=1000, help="Number of stages/epochs")
+    parser.add_argument('-nStage', type=int, default=10, help="Number of stages/epochs")
     parser.add_argument('-const', type=float, default=5, help="Weight put on the attack loss")
     parser.add_argument('-batch_size', type=int, default=5, help="Number of functions sampled for each iteration in the optmization steps")
     parser.add_argument('-rv_dist', default='UnitSphere', help="Choose from UnitSphere and UnitBall")
--- optimization_methods/FZCGS.py
+++ optimization_methods/FZCGS.py
@@ -11,44 +11,22 @@ def condg(g_0, u_0, gamma, eta, Q):
         uexp = np.repeat(np.expand_dims(u_t,axis=1),Q.shape[1],axis=1)
         vidx = np.argmax(np.dot(g_t+(1/gamma)*(u_t-u_0.reshape(-1)),-Q+uexp))
         v = Q[:,vidx]
-        v_t = np.dot(g_t+(1/gamma)*(u_t-u_0.reshape(-1)),-Q[:,vidx]+u_t)
+        dot = np.dot(g_t+(1/gamma)*(u_t-u_0.reshape(-1)),-Q[:,vidx]+u_t)
 
-        if v_t <= eta:  # v_t is indeed the Frank-Wolfe gap.
+        if dot <= eta:  # v_t is indeed the Frank-Wolfe gap.
             return u_t.reshape(u_0.shape)
         
-        norm = np.square(np.linalg.norm(v_t - u_t))
-        arg = np.inner((1/gamma) * (u_0.reshape(-1) - u_t) - g_0, v - u_t) / ((1/gamma) * norm)
-        alpha_t = np.min([1, arg])
-        u_t = (1-alpha_t) * u_t + alpha_t * v
-
-        t = t+1
-
-
-def condg(g_0, u_0, gamma, eta, Q):
-    u = u_0.reshape(-1)
-    g = g_0.reshape(-1)
-    d = len(u)
-    t = 0
-    alpha=1
-
-    while True:
-        uexp = np.repeat(np.expand_dims(u,axis=1),Q.shape[1],axis=1)
-        vidx = np.argmax(np.dot(g+(1/gamma)*(u-u_0.reshape(-1)),-Q+uexp))
-        v = Q[:,vidx]
-        dot = np.dot(g+(1/gamma)*(u-u_0.reshape(-1)),-Q[:,vidx]+u)
-        #print("dot product = " + str(dot) + " ; eta = " + str(eta))
-        if (dot <= eta) or (t == 1000000) or (alpha<0.00001):
-            print('T number in CG = ' + str(t))
-            print("final alpha: " + str(alpha))
-            print(str(dot) + " " + str(eta))
-            return u.reshape(u_0.shape)
-
-        alpha = min( gamma * np.dot((1/gamma)*(u - u_0.reshape(-1)) - g , v-u ) / (np.linalg.norm(v - u) ** 2), 1)
-        #print("alpha = " + str(alpha))
-        u = (1 - alpha)*u + alpha*v
+        #norm = np.square(np.linalg.norm(v_t - u_t))
+        #arg = np.inner((1/gamma) * (u_0.reshape(-1) - u_t) - g_0, v - u_t) / ((1/gamma) * norm)
+        #alpha_t = np.min([1, arg])
+        #u_t = (1-alpha_t) * u_t + alpha_t * v
+        #t = t+1
+        alpha = min( gamma * np.dot((1/gamma)*(u_t - u_0.reshape(-1)) - g_t , v-u_t ) / (np.linalg.norm(v - u_t) ** 2), 1)
+        u_t = (1 - alpha)*u_t + alpha*v
         t+=1
 
-def CG(g_0, u_0, eta, beta, Q):
+
+def CG_working(g_0, u_0, eta, beta, Q):
     u = u_0.reshape(-1)
     g = g_0.reshape(-1)
     d = len(u)
@@ -96,7 +74,7 @@ def FZCGS(x_0, N, q, K, L, obj_func, MGR):
     eta = 1/K # =0.1
 
     x_k = x_0.copy()
-    e = np.eye(d)
+    
 
     ### FEASIBLE SET ###
     num = 2000
@@ -106,50 +84,43 @@ def FZCGS(x_0, N, q, K, L, obj_func, MGR):
     Q= np.concatenate((Q,-Q), axis=1)
     ####################
 
-    q_val = 28 #B
-    e = np.zeros((d, q_val))
-    print(e.shape)
+    q_val = d 
+    e = np.eye(d)
+    #e = np.zeros((d, q_val))
 
-    for k in range(N): #it goes from 0 to n-1
 
-        
+    #e = np.zeros((d, q_val))
+    #for j in range(q_val):
+    #    basis_vector = np.zeros(q_val)
+    #    basis_vector[j]=1
+    #    e[j, :] = basis_vector
+
+    for k in range(N):
+
         if np.mod(k, q) == 0: 
-            S1_batch_idx = np.random.choice(np.arange(0, MGR.parSet['nFunc']), n, replace = False) #what is 50? number of elements randomly chosen
-            
+            S1_batch_idx = np.random.choice(np.arange(0, MGR.parSet['nFunc']), n, replace = False) 
             v_k = np.zeros(x_k.shape)
-            for j in range(q_val):
-                e[:, j] = 0
-                e[j, j] = 1
-                v_k += ((obj_func.evaluate(x_k + mu * e[:, j].reshape(x_k.shape), S1_batch_idx) -
-                        obj_func.evaluate(x_k - mu * e[:, j].reshape(x_k.shape), S1_batch_idx)) / (2 * mu)) * e[:, j].reshape(x_k.shape)
-                e[j, j] = 0
+            for idx in range(d):
+                (1/(2*mu))*(obj_func.evaluate(x_k + mu * e[idx,:].reshape(shape),S1_batch_idx) - obj_func.evaluate(x_k - mu * e[idx,:].reshape(shape),S1_batch_idx)) * e[idx,:].reshape(shape)
+                #print("if", v_k)
         else:
-            S2_batch_idx = np.random.choice(np.arange(0, MGR.parSet['nFunc']), q, replace=True) #false?
+            S2_batch_idx = np.random.choice(np.arange(0, MGR.parSet['nFunc']), q, replace=True) 
             v_k = np.zeros(x_k.shape)
-            for j in range(q):
-                for k in range(q_val):
-   
-                   v_k = v_k +  (
-                                    
-                                    obj_func.evaluate(x_k + mu * e[:, k], S2_batch_idx[j : j + 1])
-                                    - obj_func.evaluate(x_k - mu * e[:, k], S2_batch_idx[j : j + 1])
-                                ) * e[:, k] / (2*mu) - (
-                                    
-                                    obj_func.evaluate(x_prev + mu * e[:, k], S2_batch_idx[j : j + 1])
-                                    - obj_func.evaluate(x_prev - mu * e[:, k], S2_batch_idx[j : j + 1])
-                                ) * e[:, k] / (2*mu)
-                            
+            for qidx in range(q):
+                for idx in range(d):
+                    v += ((obj_func.evaluate(x_k + mu * e[idx,:].reshape(shape),S2_batch_idx[qidx:qidx+1]) - obj_func.evaluate(x_k - mu * e[idx,:].reshape(shape),S2_batch_idx[qidx:qidx+1])) * e[idx,:].reshape(shape)) - ((obj_func.evaluate(x_prev + mu * e[idx,:].reshape(shape),S2_batch_idx[qidx:qidx+1]) - obj_func.evaluate(x_prev - mu * e[idx,:].reshape(shape),S2_batch_idx[qidx:qidx+1])) * e[idx,:].reshape(shape))
+                #print("else: ", v_k)
 
-                v_k = v_k + v_prev
-            v_k = (1/q_val)*(v_k)
+            v = (1/q_val)*(1/(2*mu))*(v/q)+v_prev
+            
         
-        # at the first iteration of the for loop, k=0 so it doesn't jump to the else branch, and therefore v_prev and x_prev are safe to put here
+        ## at the first iteration of the for loop, k=0 so it doesn't jump to the else branch, and therefore v_prev and x_prev are safe to put here
         v_prev = v_k.copy() # is it right to copy prev here?
         x_prev = x_k.copy() # is it right to copy prev here?
 
-        #x_k = condg(v_k, x_k, gamma, eta, Q)
-        x_k = CG(v_k, x_k, gamma,eta,Q)
-        #print(x_k)
+        x_k = condg(v_k, x_k, gamma, eta, Q)
+        #x_k = CG(v, x_k, gamma,eta,Q)
+        print(x_k[:,:,0])
         
         # Insert here print current loss at iteration index
 
