37787f63d6d222df63ca598d262b5283ca523c40
Marco <marco.uderzodj@gmail.com>
2023-10-29 12:04:12 +0100

    Revised FZCGS for better readability

--- optimization_methods/FZCGS.py
+++ optimization_methods/FZCGS.py
@@ -1,32 +1,55 @@
 import numpy as np
 import random
 
-def condg(g_0, u_0, gamma, eta, Q):
+def condg(g_0, u_0, gamma, eta, OMEGA):
     u_t = u_0.reshape(-1)
     g_t = g_0.reshape(-1)
     t = 0
     alpha_t=1
 
     while(True):
-        uexp = np.repeat(np.expand_dims(u_t,axis=1),Q.shape[1],axis=1)
-        vidx = np.argmax(np.dot(g_t+(1/gamma)*(u_t-u_0.reshape(-1)),-Q+uexp))
-        v = Q[:,vidx]
-        dot = np.dot(g_t+(1/gamma)*(u_t-u_0.reshape(-1)),-Q[:,vidx]+u_t)
+        u_expanded = np.repeat(np.expand_dims(u_t,axis=1),OMEGA.shape[1],axis=1)
+        v_idx = np.argmax(np.dot(g_t+(1/gamma)*(u_t-u_0.reshape(-1)),-OMEGA+u_expanded))
+        v = OMEGA[:,v_idx]
+        dot = np.dot(g_t+(1/gamma)*(u_t-u_0.reshape(-1)),-OMEGA[:,v_idx]+u_t)
 
         if dot <= eta or alpha_t < 0.00001 or t==10000000:  # v_t is indeed the Frank-Wolfe gap.
             return u_t.reshape(u_0.shape) 
         
-        #norm = np.square(np.linalg.norm(v_t - u_t))
-        #arg = np.inner((1/gamma) * (u_0.reshape(-1) - u_t) - g_0, v - u_t) / ((1/gamma) * norm)
-        #alpha_t = np.min([1, arg])
-        #u_t = (1-alpha_t) * u_t + alpha_t * v
-        #t = t+1
-        alpha_t = min( gamma * np.dot((1/gamma)*(u_t - u_0.reshape(-1)) - g_t , v-u_t ) / (np.linalg.norm(v - u_t) ** 2), 1)
+        a_dot = np.dot((1/gamma)*(u_t - u_0.reshape(-1)) - g_t , v-u_t ) # to check if (u_t - u_0.reshape(-1)) is correct.
+        a_normsq = (np.linalg.norm(v - u_t) ** 2)
+        alpha_t = min(1, a_dot / ((1/gamma) * a_normsq))
 
-        print(str(1-alpha_t*u_t))
-        print(str(alpha_t*v))
         u_t = (1 - alpha_t)*u_t + alpha_t*v
             
+        t+=1
+
+
+def CG_working(g_0, u_0, eta, beta, Q):  # Another implementation of CG from another paper. Use this for benchmarking with condg.
+    u = u_0.reshape(-1)
+    g = g_0.reshape(-1)
+    d = len(u)
+    t = 0
+    alpha_t=1
+
+    while True:
+        vidx = np.argmin(np.dot(g,Q))
+        v=Q[:,vidx]
+
+        #v = -np.sign(g)*4
+
+        dot=np.dot(g, u - v)
+        #print(str(dot) + " " + str(beta))
+        if (dot <= beta) or (t==1000000) or (alpha_t<0.00001):
+            print('T number in CG = ' + str(t))
+            print("final alpha_t: " + str(alpha_t))
+            print(str(dot) + " " + str(beta))
+            #print(u.reshape((28,28)))
+            return u.reshape(u_0.shape)
+        alpha_t = min(np.dot(g, u - v) / (eta * np.linalg.norm(u - v) ** 2), 1)
+        #print("alpha_t: " + str(alpha_t))
+        u = u + alpha_t * (v - u)
+        g = g_0.reshape(-1) + eta * (u - u_0.reshape(-1))
         t+=1
 
 
@@ -52,12 +75,19 @@ def FZCGS(x_0, N, q, K, L, obj_func, MGR):
     x_k = x_0.copy()
     
 
-    ### FEASIBLE SET ###
+
+    ### FEASIBLE SET ### 
+
+    # This feasible set contains all possible directions in which sum of elements 
+    # is equal to 0 (all elems except ones equal to 2000/d and their opposite
+    # We need to implement ||delta||_inf <= s, as in the Gao et al. paper
+
     num = 2000
-    Q = np.eye(d)*num
-    Qrm = np.full(Q.shape, num/d)
-    Q = Q - Qrm
-    Q= np.concatenate((Q,-Q), axis=1)
+    OMEGA = np.eye(d)*num
+    OMEGA_rm = np.full(OMEGA.shape, num/d)
+    OMEGA = OMEGA - OMEGA_rm
+    OMEGA= np.concatenate((OMEGA,-OMEGA), axis=1)
+
     ####################
 
     q_val = d 
@@ -65,27 +95,33 @@ def FZCGS(x_0, N, q, K, L, obj_func, MGR):
     #e = np.zeros((d, q_val))
 
 
-    #e = np.zeros((d, q_val))
-    #for j in range(q_val):
-    #    basis_vector = np.zeros(q_val)
-    #    basis_vector[j]=1
-    #    e[j, :] = basis_vector
 
-    for k in range(N): # iterations
+    for k in range(50): # iterations, change to N
 
         if np.mod(k, q) == 0: 
             S1_batch_idx = np.random.choice(np.arange(0, MGR.parSet['nFunc']), n, replace = False) 
             v_k = np.zeros(x_k.shape)
-            for idx in range(d): # loops 28x28 times 
-                v_k += (1/(2*mu))*(obj_func.evaluate(x_k + mu * e[idx,:].reshape(shape),S1_batch_idx) - obj_func.evaluate(x_k - mu * e[idx,:].reshape(shape),S1_batch_idx)) * e[idx,:].reshape(shape)
+            for j in range(d): # loops 28x28 times 
+                v_k += (1 / (2 * mu)) * (
+                                            obj_func.evaluate(x_k + mu * e[j, :].reshape(shape), S1_batch_idx) -
+                                            obj_func.evaluate(x_k - mu * e[j, :].reshape(shape), S1_batch_idx)
+                                        ) * e[j, :].reshape(shape)
                 #if np.mod(k, 100) == 0:
                 #    print(v_k)
         else:
             S2_batch_idx = np.random.choice(np.arange(0, MGR.parSet['nFunc']), q, replace=True) 
             v_k = np.zeros(x_k.shape)
-            for qidx in range(q):
-                for idx in range(d): # loops 28x28 times
-                    v_k += ((obj_func.evaluate(x_k + mu * e[idx,:].reshape(shape),S2_batch_idx[qidx:qidx+1]) - obj_func.evaluate(x_k - mu * e[idx,:].reshape(shape),S2_batch_idx[qidx:qidx+1])) * e[idx,:].reshape(shape)) - ((obj_func.evaluate(x_prev + mu * e[idx,:].reshape(shape),S2_batch_idx[qidx:qidx+1]) - obj_func.evaluate(x_prev - mu * e[idx,:].reshape(shape),S2_batch_idx[qidx:qidx+1])) * e[idx,:].reshape(shape))
+            for i in range(q):
+                for j in range(d): # loops 28x28 times
+                    v_k += (
+                                (obj_func.evaluate(x_k + mu * e[j, :].reshape(shape), S2_batch_idx[i:i+1]) -
+                                obj_func.evaluate(x_k - mu * e[j, :].reshape(shape), S2_batch_idx[i:i+1])) *
+                                e[j, :].reshape(shape)
+                            ) - (
+                                (obj_func.evaluate(x_prev + mu * e[j, :].reshape(shape), S2_batch_idx[i:i+1]) -
+                                obj_func.evaluate(x_prev - mu * e[j, :].reshape(shape), S2_batch_idx[i:i+1])) *
+                                e[j, :].reshape(shape)
+                            )
                 #if np.mod(k, 100) == 0:
                 #    print(v_k)
 
@@ -93,12 +129,12 @@ def FZCGS(x_0, N, q, K, L, obj_func, MGR):
             
         
         ## at the first iteration of the for loop, k=0 so it doesn't jump to the else branch, and therefore v_prev and x_prev are safe to put here
-        v_prev = v_k.copy() # is it right to copy prev here?
-        x_prev = x_k.copy() # is it right to copy prev here?
+        v_prev = v_k.copy() 
+        x_prev = x_k.copy() 
 
-        x_k = condg(v_k, x_k, gamma, eta, Q)
-        #x_k = CG(v, x_k, gamma,eta,Q)
-        print("--------------------> AFTER condg:", x_k[:,:,0])
+        x_k = condg(v_k, x_k, gamma, eta, OMEGA)
+        #x_k = CG_working(v, x_k, gamma,eta,Q)
+        print("AFTER condg:", x_k[:,:,0])
         
         # Insert here print current loss at iteration index
 
